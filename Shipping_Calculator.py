e3280765260732202b58a7289f84e007163478be
ananyasaigal001 <50635283+ananyasaigal001@users.noreply.github.com>
2020-05-08 17:11:09 -0400

    Shipment Cost

--- Shipping_Calculator.py
+++ Shipping_Calculator.py
@@ -1,7 +1,10 @@
 
-def shipping_calculor(n):
+def shipping_calculator(n):
     return (10.95+2.95*(n-1))
 
 #number of items inputted
 number=int(input("The number of items: "))
-print("For",number,"items,the shipping cost will be $",shipping_calculor(number))
+
+#Shipment cost
+cost=round(shipping_calculator(number),2)
+print("For",number,"items,the shipping cost will be $",cost)
